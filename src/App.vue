12d35ce475f414c1ad94f5fa13dff2d8a85dd4eb
Xin-hai <aa1940358283@163.com>
2023-08-15 06:00:43 +0800

    完成API组件

--- src/App.vue
+++ src/App.vue
@@ -1,37 +1,40 @@
 <template>
   <div id="app">
       <Header />
-    <router-view name="main"></router-view>
-
+    <router-view name="main" :key="$route.fullPath"></router-view>
+    <Footer />
   </div>
 </template>
 
 <script>
 import Header from '@/views/Header.vue'
+import Footer from "@/components/Footer.vue";
 
 
 export default {
   name: 'APP',
   components:{
+    Footer,
     Header,
   }
 }
 </script>
 
-<style lang="scss">
+<style lang="scss" >
 @import '~@/assets/style/helper.scss';
 @import '~@/assets/style/reset.scss';
+@import '~@/assets/style/markdown.css';
 #app {
-  font-family: "Helvetica Neue","Luxi Sans","DejaVu Sans",$font-fangSong,$font-song,$font-kai;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
 
 }
 body {
   word-break: break-word;
-  line-height: 20px;
+  line-height: 2em;
   color: #333;
   background: #e1e1e1;
+  font-family: "Helvetica Neue","Luxi Sans","DejaVu Sans",Tahoma,"Hiragino Sans GB",STHeiti,sans-serif!important;;
 }
 .loading {
   text-align: center;
